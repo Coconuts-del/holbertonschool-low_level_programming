29ca8dfb77e150c64f1bd4bb8fa0d80776dce9f5
root <enidaucl@gmail.com>
2023-06-21 10:01:04 -0700

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - print all natural number form nb1 to 98
+ * @n: integer
+ * Return: (void)
+ */
+
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+			}
+			else
+			{
+				printf("%d,", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+			}
+			else
+			{
+				printf("%d,", n);
+			}
+		}
+	}
+}
