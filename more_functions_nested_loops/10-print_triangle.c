8f026f6413102df938763b7633fd12a5e14d4390
root <enidaucl@gmail.com>
2023-06-23 12:18:30 -0700

    10-triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that draws a diagonanl line in the terminal
+ * @size: number time to draw #
+ * Return: (void)
+ */
+
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if (j >= i)
+			{
+				_putchar('#');
+				if (j == size)
+				{
+					_putchar('\n');
+				}
+			}
+			else
+			{
+				_putchar('');
+			}
+		}
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
