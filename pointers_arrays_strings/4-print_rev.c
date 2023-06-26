d27f4d0a11debb31e9643277906d740e10ccd24c
root <enidaucl@gmail.com>
2023-06-26 07:20:44 -0700

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,20 +4,16 @@
 * print_rev - prints a string,in reverse, followed by a new line,
 *
 * @s : s = valeur de s *s = address of s
-* Return: (void))
+* Return: (void)
 */
 
 void print_rev(char *s)
 {
 	int i;
-	int j;
 
-	for (i = 0; (s[i] != '\0'); i++)
+	for (i = _strlen(s) - 1; i >= 0; i--)
 	{
-	}
-	for (j = i ; j >= 0; j--)
-	{
-		_putchar (s[j]);
+		_putchar (s[i]);
 	}
 	_putchar ('\n');
 }
