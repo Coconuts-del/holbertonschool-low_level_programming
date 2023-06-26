dcce44a462a34fbbc02eb0e897bd52930924648d
root <enidaucl@gmail.com>
2023-06-25 23:05:52 -0700

    0-reset_to_98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - int pointer as parameter and updates the value it points to 98
+*
+* @n : n = valeur de n *n = address of n
+* Return (void)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
