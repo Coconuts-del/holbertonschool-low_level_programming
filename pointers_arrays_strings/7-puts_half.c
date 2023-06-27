79ef16fc02eac326bb7790b217cd25af490edf88
root <enidaucl@gmail.com>
2023-06-27 00:11:52 -0700

    7-puts_half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* puts_half - prints a string, followed by a new line, to stdout
+*
+* @str : str = valeur de str *str = address of str
+* Return: (void)
+*/
+
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	for (i = 0; (str[i] != '\0'); i++)
+	{
+	}
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i - 1) / 2;
+
+	for (k = j; k < i; k++)
+	{
+		_putchar (str[k]);
+	}
+	_putchar ('\n');
+}
