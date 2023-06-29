52f516b1d29d81467b40f5b4a1f9f610d59f565a
root <enidaucl@gmail.com>
2023-06-29 13:56:30 -0700

    4-rev_array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _reverse_array - reverses the content of an array of integers 
+* _reverse_array - reverses the content of an array of integers
 *
 * @a   :  an array of integer
 * @n   :  number of elements of the array
@@ -9,14 +9,15 @@
 */
 
 void reverse_array(int *a, int n)
-{ int *ptr1,*ptr2;
-  int tmp; 
-  ptr1 = &a[0];  
-  ptr2 = &(a[n - 1]);
-  for ( ; ptr1 < ptr2; ptr1++, ptr2--)
-  {  
-    tmp = *ptr1;
-    *ptr1 = *ptr2;
-    *ptr2 = tmp;
-  }  
+{
+	int *ptr1, *ptr2;
+	int tmp;
+	ptr1 = &a[0];
+	ptr2 = &(a[n - 1]);
+	for ( ; ptr1 < ptr2; ptr1++, ptr2--)
+	{
+		tmp = *ptr1;
+		*ptr1 = *ptr2;
+		*ptr2 = tmp;
+	}
 }  
