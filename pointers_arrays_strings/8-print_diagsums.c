675cc431e6f99575deab7f426a46da832ec5ad7c
root <enidaucl@gmail.com>
2023-06-30 13:48:23 -0700

    diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,18 +10,21 @@
 
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, i;
+	int i, sum1, sum2, step1, step2;
 
 	sum1 = 0;
 	sum2 = 0;
-	for (i = 0; i < size;  i++)
+
+	for (i = 0; i <= (size - 1); i++)
 	{
-		sum1 = sum1 + a[i * size + i];
+		step1 =  (size + 1) * i;
+		sum1 = sum1 + *(a + step1);
 	}
 
-	for (i = size - i; i >= 0; i--)
+	for (i = 1; i <= size; i++)
 	{
-		sum2 = sum2 + a[i * size + (size - i - 1)];
+		step2 = (size - 1) * i;
+		sum2 = sum2 + *(a + step2);
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
