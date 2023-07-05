c63dd2356dc64d13e678f76c61ed57a654e4c2fd
root <enidaucl@gmail.com>
2023-07-05 07:18:14 -0700

    3-mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,10 +9,15 @@
 */
 
 
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc), char **argv)
 {
 	int sum;
 
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	sum = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", sum);
 	return (0);
