6ed272238b657ba4b79fe8b443615c2cede4b09b
root <enidaucl@gmail.com>
2023-07-06 04:07:08 -0700

    4-add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main -  program that add positive numbers
@@ -11,19 +12,22 @@
 
 int main(int argc, char **argv)
 {
-	int i;
-	long sum;
+	int i, j;
+	int sum;
 
 	sum = 0;
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			sum += atoi(argv[i]);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		printf("%ld\n", sum);
+		sum += atoi(argv[i]);
 	}
-	else
-		printf("0\n");
+	printf("%d\n", sum);
 	return (0);
 }
