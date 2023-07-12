43726bcf68cb8881e6962ab1f96500f58661c166
root <enidaucl@gmail.com>
2023-07-12 10:38:20 -0700

    0-malloc_checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked  - allocates memory using malloc
+ *
+ * @b : size memory to allocate
+ * Return: pointeur (success) exit 98.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
+
+
