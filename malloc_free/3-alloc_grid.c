877980a599c8d1171f2f4147926fc5282c1aabdd
root <enidaucl@gmail.com>
2023-07-12 05:52:27 -0700

    3-alloc_grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * initialized to 0
+ *
+ * @width : number columns of the array
+ * @height : number of rows
+ * Return:  pointer to the array(success) NULL (failed)
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <=  0 || height <= 0)
+		return (NULL);
+	grid = (int **) malloc(height * sizeof(int*));
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	
+	return (grid);
+}
+
--- malloc_free/README.md
+++ malloc_free/README.md
@@ -4,3 +4,4 @@ C - malloc, free
 1 - given as a parameter.	
 2 - The returned pointer should point to a newly allocated space in memory which contains 
 2 -	the contents of s1, followed by the contents of s2, and null terminated
+3 - function that returns a pointer to a 2 dimensional array of integers
