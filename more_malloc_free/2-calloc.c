1a9e214711824846272ff0fcd12a1f645b2676f4
root <enidaucl@gmail.com>
2023-07-14 02:40:41 -0700

    2-calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * _calloc  - allocates memory array using malloc,call calloc if malloc failed
+ * _calloc  - allocates memory array using malloc,
  *
  * @nmemb : number of elements
  * @size  : size of each element
@@ -13,18 +13,34 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
-	{
-		if (nmemb == 0 || size == 0)
-			return (NULL);
-
-		ptr = calloc(nmemb, size);
-		if (ptr == NULL)
-			return (NULL);
-
-	}
+		return (NULL);
+	_memset(ptr, 0, (nmemb * size));
 	return (ptr);
 }
 
+/**
+* _memset - fills memory with a constant byte
+*
+* @s    : pointeur
+* @b    : constante a mettre dans les n bytes to the memory area s
+* @n    : nbre de bytes a modifier
+*
+* Return: (char)
+*/
 
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
