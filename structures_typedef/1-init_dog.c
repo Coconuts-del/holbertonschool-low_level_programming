d723e2365c747ae09ec083575dbc791d7524f2b8
root <enidaucl@gmail.com>
2023-07-17 04:57:18 -0700

    error on initialisation

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -15,8 +15,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != 0)
 	{
-	(*d).name = NULL;
-	(*d).age = NULL;
-	(*d).owner = NULL;
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 	}
 }
