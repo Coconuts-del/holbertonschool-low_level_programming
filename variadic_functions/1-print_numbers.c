ebd5b1894837e931c1b916a9eae3fc2d7a0b716d
root <enidaucl@gmail.com>
2023-07-21 01:15:48 -0700

    print separator ?

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - sum of all its parameters
+ * print_numbers - sum of all its parameters
  * @separator : argument contains string to be printed between arguments
  * @n :  argument contains number of arguments of variadic argument
  * @... : variadic argument
@@ -17,8 +17,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		va_start(ap, n);
 		for (i = 0; i < n; i++)
-			printf("%d%s", va_arg(ap, int), separator);
+		{
+			if (n == (i + 1))
+				printf("%d", va_arg(ap, int));
+			else
+				printf("%d%s", va_arg(ap, int), separator);
+		}
 		va_end(ap);
+		printf("\n");
 	}
-	printf("\n");
 }
