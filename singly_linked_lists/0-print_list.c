55c716987b09d33f8f264b58d252c5bb1de29976
root <enidaucl@gmail.com>
2023-08-01 01:19:15 -0700

    betty style on 0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 /**
  * print_list -  prints all elements of a list_t list
  * @h  : pointer to the head of a list
- * Return : number of elements of a list
+ * Return: number of elements of a list
 */
 
 size_t print_list(const list_t *h)
