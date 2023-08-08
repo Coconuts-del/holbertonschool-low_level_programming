ef709b6749614f5e7fbf7d0e1914301ac8902043
root <enidaucl@gmail.com>
2023-08-08 11:20:19 -0700

    test close

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0;
+	int fd, i = 0, j = 0;
 
 	if (!filename)
 		return (-1);
@@ -21,8 +21,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i];)
 			i++;
-		write(fd, text_content, i);
+		j = write(fd, text_content, i);
 	}
-	close(fd);
+	if (close(fd) == -1 || j == -1)
+		return (-1);
 	return (1);
 }
