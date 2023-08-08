000ec9c16c20251e00f108034929b58c9a9334aa
root <enidaucl@gmail.com>
2023-08-08 10:53:26 -0700

    NULL TEST

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,18 +9,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i, j;
+	int fd, i = 0;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	for (i = 0; text_content[i];)
-		i++;
-	j = write(fd, text_content, i);
-	if (close(fd) == -1 || i != j)
-		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i];)
+			i++;
+		write(fd, text_content, i);
+	}
+	close(fd);
 	return (1);
 }
