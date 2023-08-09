38d18079e385a363832898ab7b92d95084695b0c
root <enidaucl@gmail.com>
2023-08-09 03:39:55 -0700

    update

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,7 +9,7 @@ int closeFic(int fd)
 {
 	if (close(fd) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		return (100);
 	}
 	return (0);
@@ -31,34 +31,32 @@ int main(int ac, char **av)
 	char buffer[1024];
 
 	if (ac != 3)
-	{   dprintf(2, "Usage: cp file_from file_to\n");
-		return (97);
-	}
+	{   dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97); }
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
-	{   dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		return (98);
-	}
+	{   dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98); }
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
-	{   dprintf(2, "Error: Can't write to %s\n", av[2]);
+	{   dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		closeFic(fd1);
-		return (99);
+		exit(99);
 	}
 
 	do {
 		rd = read(fd1, buffer, 1024);
 		wr = write(fd2, buffer, rd);
 	} while (rd == 1024);
+
 	if (rd < 0)
-	{   dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		return (98);
-	}
+	{   dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98); }
 	if (wr < 0)
-	{	dprintf(2, "Error: Can't write to %s\n", av[2]);
-		return (99);
-	}
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99); }
+
 	if (closeFic(fd1) !=  0  ||  closeFic(fd2) != 0)
-		return (100);
+		exit(100);
 	return (0);
 }
