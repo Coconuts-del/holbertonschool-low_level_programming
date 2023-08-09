e41356043aea93688ebb2bbc6c75f5c727cfe924
root <enidaucl@gmail.com>
2023-08-09 04:55:26 -0700

    update

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -36,12 +36,12 @@ int main(int ac, char **av)
 	}
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
-	{   dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		return (98);
 	}
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
-	{   dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		return (99);
 	}
 
@@ -51,7 +51,7 @@ int main(int ac, char **av)
 	} while (rd == 1024);
 
 	if (rd < 0)
-	{   dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		return (98);
 	}
 	if (wr < 0)
