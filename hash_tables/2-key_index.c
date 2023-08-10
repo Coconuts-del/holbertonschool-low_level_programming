18988871a5725749bbb6e20500a2806ff41918f4
root <enidaucl@gmail.com>
2023-08-10 13:17:24 -0700

    betty style

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -3,7 +3,7 @@
 /**
  * key_index - function gives the index of a key
  * @key : string used to generate hash value
- * size : size of the array
+ * @size : size of the array
  * Return: index (hash value % size)
  */
 
