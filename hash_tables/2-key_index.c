77df1e985d899e84810f93ea152cc83e0d314268
root <enidaucl@gmail.com>
2023-08-10 13:14:14 -0700

    2-key_index.c

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function gives the index of a key
+ * @key : string used to generate hash value
+ * size : size of the array
+ * Return: index (hash value % size)
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int idx;
+
+	idx = hash_djb2((unsigned char *)key);
+	return (idx % size);
+}
